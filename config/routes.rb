119060c277c8b45571932530a5e3cd94b41be780
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-11 09:09:00 -0500

    5.4.2: use users controller for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "static_pages#home"
@@ -7,4 +6,6 @@ Rails.application.routes.draw do
   get "/about",   to: "static_pages#about"
   get "/help",    to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
+
+  get "/signup", to: "users#new"
 end
