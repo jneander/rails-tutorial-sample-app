eb798349deafa7ae38abf25d9a41bf6cc8b9e3f6
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-11 17:32:00 -0500

    7.3.2: save user with valid params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
