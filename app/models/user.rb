285521be9f47cb033eb78e23cbbb3966088cab33
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-14 07:33:25 -0500

    10.1.4: complete user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
             length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: true
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
   before_save { email.downcase! }
 
