8d8546ff599e352a27d370b10659e7827338d204
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-16 07:27:19 -0500

    13.1.3: link user to have many microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_many :microposts
+
   validates :name, presence: true, length: {maximum: 50}
   validates :email,
             presence: true,
