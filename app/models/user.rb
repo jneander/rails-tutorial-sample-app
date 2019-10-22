e2124c0fc71b0e0d7c7a052a9bd2c9d5817d1de9
Jeremy Neander <jeremy.neander@gmail.com>
2019-10-21 21:03:29 -0500

    14.1.5: add followers association

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@ class User < ApplicationRecord
              dependent: :destroy,
              foreign_key: "follower_id"
            }
+  has_many :passive_relationships,
+           {
+             class_name: "Relationship",
+             dependent: :destroy,
+             foreign_key: "followed_id"
+           }
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: {maximum: 50}
   validates :email,
